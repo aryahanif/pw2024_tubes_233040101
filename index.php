fc27290099251895b0d7e3b2b580604b797bb6b3
Arya Raihan Hanif <aryaraihanhanif@gmail>
2024-05-14 11:59:58 +0700

    Progress part 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php  
+// Koneksi Database
+$db = mysqli_connect('localhost', 'root', '', 'pw2024_tubes_233040101')
+
+// Query ke tabel mahasiswa
+
+
+// Menyimpan data mahasiswa
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PerpusBL</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <container>
+        <div class= "jawa">
+            <h2>Akulah Raden Kian Santang</h2>
+        </div>
+    </container>
+    
+</body>
+</html>
